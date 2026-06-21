bf039a9a456513fd0be072a0c2a08b712b015bb6
Motak24 <petr240907@gmail.com>
2026-06-21 06:43:35 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const words = [
+  "абзац",
+  "арбуз",
+  "банка",
+  "белка",
+  "ветер",
+  "волна",
+  "город",
+  "грива",
+  "дверь",
+  "дождь",
+  "ездок",
+  "завод",
+  "звезд",
+  "игрок",
+  "книга",
+  "крыло",
+  "носок",
+  "океан",
+];
+
+
+const App = () => {
+    const root = document.getElementById('root');
+
+    const h1  = document.createElement('h1');
+    h1.innerText = 'Угадай слово'
+    root.append(h1);
+
+    const 
+
+} 
+
+App();
